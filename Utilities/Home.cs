49a5d33aa030acc001d11eb8299a525b107b63f0
ArtlessBoi <jayanlas00@gmail.com>
2023-04-03 09:29:17 -0400

    added the classes and forms but didnt want to change to master

---
+++ Utilities/Home.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class Home
+    {
+        int _home_id, _zip_code, _house_size;
+        float _price, _number_bed, _number_bath, _rating;
+        string _seller_email, _address, _property_type, _other_amenities, _status   ;
+
+        public Home() { }
+
+        //properties
+
+
+
+        //functions
+    }
+}
