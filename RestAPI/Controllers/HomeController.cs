4e0ec3f8b46ea0cb95f43b97d5b086d99f025706
jisanjung <jjjung13@gmail.com>
2023-04-04 13:47:00 -0400

    configure: reference Utilites to RestAPI

--- RestAPI/Controllers/HomeController.cs
+++ RestAPI/Controllers/HomeController.cs
@@ -5,12 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.Data;
+using Utilities;
+
 namespace RestAPI.Controllers
 {
     [Route("api/[controller]")]
     // [ApiController]
     public class HomeController : Controller
     {
+        DBConnect connection = new DBConnect();
+
         [HttpGet]
         public string Test()
         {
