9f2d992d8778757323fb22c1564dee8053ba0d97
jisanjung <jjjung13@gmail.com>
2023-04-04 14:36:33 -0400

    feature: rest api method to get all homes

--- RestAPI/Controllers/HomeController.cs
+++ RestAPI/Controllers/HomeController.cs
@@ -6,8 +6,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using System.Data;
+using System.Data.SqlClient;
 using Utilities;
-using HomeLibrary;
+using HomeClassLibrary;
 
 namespace RestAPI.Controllers
 {
@@ -18,10 +19,33 @@ namespace RestAPI.Controllers
         DBConnect connection = new DBConnect();
 
         [HttpGet]
-        public string Test()
+        public List<Home> GetAllHomes()
         {
-            
-            return "hi";
+            SqlCommand objCommand = new SqlCommand();
+            List<Home> homes = new List<Home>();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetAllHomes";
+
+            DataSet ds = connection.GetDataSet(objCommand);
+            DataTable dt = ds.Tables[0];
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                Home h = new Home();
+                h.HomeId = int.Parse(dt.Rows[i]["home_id"].ToString());
+                h.SellerEmail = dt.Rows[i]["seller_email"].ToString();
+                h.Price = float.Parse(dt.Rows[i]["price"].ToString());
+                h.Address = dt.Rows[i]["address"].ToString();
+                h.ZipCode = int.Parse(dt.Rows[i]["zip_code"].ToString());
+                h.PropertyType = dt.Rows[i]["property_type"].ToString();
+                h.HouseSize = int.Parse(dt.Rows[i]["house_size"].ToString());
+                h.NumberBed = int.Parse(dt.Rows[i]["number_bed"].ToString());
+                h.NumberBath = int.Parse(dt.Rows[i]["number_bath"].ToString());
+                h.OtherAmenities = dt.Rows[i]["other_amenities"].ToString();
+                h.Rating = int.Parse(dt.Rows[i]["rating"].ToString());
+                h.Status = dt.Rows[i]["status"].ToString();
+                homes.Add(h);
+            }
+            return homes;
         }
     }
 }
