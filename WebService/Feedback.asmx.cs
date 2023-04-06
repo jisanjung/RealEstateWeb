a15d1948d51b6b1303dd404514615a13d77f32e9
Jisan Jung <jjjung13@gmail.com>
2023-04-06 00:18:16 -0400

    feature: add service method to get all feedback for specified home

--- WebService/Feedback.asmx.cs
+++ WebService/Feedback.asmx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
 
 using Utilities;
 using HomeLibrary;
@@ -19,11 +21,35 @@ namespace WebService
     // [System.Web.Script.Services.ScriptService]
     public class Feedback : System.Web.Services.WebService
     {
+        DBConnect connection = new DBConnect();
 
         [WebMethod]
         public string HelloWorld()
         {
             return "Hello World";
+        }
+        [WebMethod]
+        public List<HomeFeedback> GetAllFeedbackForHome(int home_id)
+        {
+            SqlCommand objCommand = new SqlCommand();
+            List<HomeFeedback> feedback = new List<HomeFeedback>();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetAllFeedbackForHome";
+
+            DataSet ds = connection.GetDataSet(objCommand);
+            DataTable dt = ds.Tables[0];
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                HomeFeedback hf = new HomeFeedback();
+                hf.FeedbackId = int.Parse(dt.Rows[i]["feedback_id"].ToString());
+                hf.HomeId = int.Parse(dt.Rows[i]["home_id"].ToString());
+                hf.PriceFeedback = dt.Rows[i]["price_feedback"].ToString();
+                hf.LocationFeedback = dt.Rows[i]["location_feedback"].ToString();
+                hf.OverallFeedback = dt.Rows[i]["overall_feedback"].ToString();
+                hf.Rating = int.Parse(dt.Rows[i]["rating"].ToString());
+                feedback.Add(hf);
+            }
+            return feedback;
         }
     }
 }
