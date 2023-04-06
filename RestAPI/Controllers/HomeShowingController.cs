a02c04939a7bb24a186e203d57d9cb8b86b580ba
Jisan Jung <jjjung13@gmail.com>
2023-04-05 22:08:30 -0400

    feature: add HomeShowing controller

---
+++ RestAPI/Controllers/HomeShowingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Utilities;
+using HomeLibrary;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    // [ApiController]
+    public class HomeShowingController : Controller
+    {
+        DBConnect connection = new DBConnect();
+    }
+}
