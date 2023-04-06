4274d1b20dcb905a753532772c8f8819c255bc46
Jisan Jung <jjjung13@gmail.com>
2023-04-05 22:48:22 -0400

    feature: create controller for home offers

---
+++ RestAPI/Controllers/HomeOffersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+using Utilities;
+using HomeLibrary;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    // [ApiController]
+    public class HomeOffersController : Controller
+    {
+        DBConnect connection = new DBConnect();
+    }
+}
