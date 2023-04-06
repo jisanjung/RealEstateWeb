889854f47740f31620dabdebb610a81444f64d01
Jisan Jung <jjjung13@gmail.com>
2023-04-05 23:02:08 -0400

    feature: api method to get all home offers

--- RestAPI/Controllers/HomeOffersController.cs
+++ RestAPI/Controllers/HomeOffersController.cs
@@ -17,5 +17,33 @@ namespace RestAPI.Controllers
     public class HomeOffersController : Controller
     {
         DBConnect connection = new DBConnect();
+
+        [HttpGet]
+        public List<HomeOffer> GetAllHomeOffers()
+        {
+            SqlCommand objCommand = new SqlCommand();
+            List<HomeOffer> homeOffers = new List<HomeOffer>();
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetAllHomeOffers";
+
+            DataSet ds = connection.GetDataSet(objCommand);
+            DataTable dt = ds.Tables[0];
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                HomeOffer ho = new HomeOffer();
+                ho.HomeOfferId = int.Parse(dt.Rows[i]["offer_id"].ToString());
+                ho.HomeId = int.Parse(dt.Rows[i]["home_id"].ToString());
+                ho.SaleType = dt.Rows[i]["sale_type"].ToString();
+                ho.Contingencies = dt.Rows[i]["contingencies"].ToString();
+                ho.OfferAmount = float.Parse(dt.Rows[i]["offer_amount"].ToString());
+                ho.MoveInDate = dt.Rows[i]["movein_date"].ToString();
+                ho.SellHomeFirst = bool.Parse(dt.Rows[i]["sell_home_first"].ToString());
+                ho.BuyerEmail = dt.Rows[i]["buyer_email"].ToString();
+                ho.SellerEmail = dt.Rows[i]["seller_email"].ToString();
+                ho.Accepted = bool.Parse(dt.Rows[i]["accepted"].ToString());
+                homeOffers.Add(ho);
+            }
+            return homeOffers;
+        }
     }
 }
