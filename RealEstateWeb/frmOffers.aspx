519ab967842d15c45ed1624b3990af758d9e6185
Jisan Jung <jjjung13@gmail.com>
2023-04-12 22:19:11 -0400

    feature: display home offer data

--- RealEstateWeb/frmOffers.aspx
+++ RealEstateWeb/frmOffers.aspx
@@ -9,6 +9,56 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <h1>Offers made to your Homes</h1>
+            <div id="divViewOffer" runat="server" visible="false">
+                <div>
+                    <asp:Label ID="lblViewOfferId" runat="server"></asp:Label>
+                </div>
+                <h4>
+                    <asp:Label ID="lblOfferTitle" runat="server"></asp:Label>
+                </h4>
+                <div>
+                    <asp:Label ID="lblBuyerEmail" runat="server"></asp:Label>
+                </div>
+                <div>
+                    <asp:Label ID="lblSaleType" runat="server"></asp:Label>
+                </div>
+                <div>
+                    <asp:Label ID="lblContingencies" runat="server"></asp:Label>
+                </div>
+                <div>
+                    <asp:Label ID="lblMoveinDate" runat="server"></asp:Label>
+                </div>
+                <div>
+                    <asp:Label ID="lblSellHomeFirst" runat="server"></asp:Label>
+                </div>
+                <asp:Button ID="btnAcceptAmount" runat="server" />
+            </div>
+            <table>
+                <tr>
+                    <th>Offer ID</th>
+                    <th>From</th>
+                    <th>Offer Amount</th>
+                </tr>
+                <asp:Repeater ID="rptOffers" runat="server">
+                    <ItemTemplate>
+                        <tr>
+                            <td>
+                                <asp:Label ID="lblOfferId" runat="server" Text='<%# DataBinder.Eval(Container.DataItem, "HomeOfferId")%>'></asp:Label>
+                            </td>
+                            <td>
+                                <asp:Label ID="lblBuyer" runat="server" Text='<%# DataBinder.Eval(Container.DataItem, "BuyerEmail")%>'></asp:Label>
+                            </td>
+                            <td>
+                                <asp:Label ID="lblOfferAmount" runat="server" Text='<%# DataBinder.Eval(Container.DataItem, "OfferAmount")%>'></asp:Label>
+                            </td>
+                            <td>
+                                <asp:Button ID="btnViewOffer" runat="server" Text="View" CommandArgument='<%# Container.ItemIndex %>' OnCommand="btn_ViewOffer"/>
+                            </td>
+                        </tr>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </table>
         </div>
     </form>
 </body>
--- RealEstateWeb/frmOffers.aspx.cs
+++ RealEstateWeb/frmOffers.aspx.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
+using Utilities;
+using HomeLibrary;
 
 namespace RealEstateWeb
 {
@@ -11,7 +15,53 @@ namespace RealEstateWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string jsonOffers = RestClient.Get("http://localhost:60855/api/homeoffers");
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            List<HomeOffer> allHomeOffers = js.Deserialize<List<HomeOffer>>(jsonOffers);
+            List<HomeOffer> unacceptedOffers = new List<HomeOffer>();
 
+            foreach (HomeOffer ho in allHomeOffers)
+            {
+                if (!ho.Accepted)
+                {
+                    unacceptedOffers.Add(ho);
+                }
+            }
+
+            if (!IsPostBack)
+            {
+                this.displayOffers(unacceptedOffers);
+            }
+        }
+        protected void btn_ViewOffer(object sender, CommandEventArgs e)
+        {
+            this.divViewOffer.Visible = true;
+
+            int rowClicked = int.Parse(e.CommandArgument.ToString());
+            Label lblOfferId = (Label)rptOffers.Items[rowClicked].FindControl("lblOfferId");
+            int offerId = int.Parse(lblOfferId.Text);
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonOffer = RestClient.Get("http://localhost:60855/api/homeoffers/" + offerId);
+            HomeOffer homeOffer = js.Deserialize<HomeOffer>(jsonOffer);
+
+
+            string jsonHome = RestClient.Get("http://localhost:60855/api/homes/" + homeOffer.HomeId);
+            Home home = js.Deserialize<Home>(jsonHome);
+
+            this.lblViewOfferId.Text = homeOffer.HomeOfferId.ToString();
+            this.lblOfferTitle.Text = $"Offer for {home.Address}";
+            this.lblBuyerEmail.Text = $"{homeOffer.BuyerEmail} wants to buy this home";
+            this.lblSaleType.Text = $"{homeOffer.SaleType}";
+            this.lblContingencies.Text = $"{homeOffer.Contingencies}";
+            this.lblMoveinDate.Text = $"{homeOffer.MoveInDate}";
+            this.lblSellHomeFirst.Text = homeOffer.SellHomeFirst ? "Buyer needs to sell their home first" : "Buyer does not need to sell their home";
+            this.btnAcceptAmount.Text = "Accept " + homeOffer.OfferAmount.ToString("c");
+        }
+        private void displayOffers(List<HomeOffer> homeOffers)
+        {
+            this.rptOffers.DataSource = homeOffers;
+            this.rptOffers.DataBind();
         }
     }
 }
