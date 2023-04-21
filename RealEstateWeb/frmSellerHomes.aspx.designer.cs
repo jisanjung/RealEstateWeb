6f8dcc3e54eae9b2391be92de4c8863606fc8e25
Jisan Jung <jjjung13@gmail.com>
2023-04-21 02:06:07 -0400

    style: fix seller homes

--- RealEstateWeb/frmSellerHomes.aspx.designer.cs
+++ RealEstateWeb/frmSellerHomes.aspx.designer.cs
@@ -186,6 +186,15 @@ namespace RealEstateWeb
         protected global::System.Web.UI.WebControls.LinkButton linkbtnCloseFeedback;
 
         /// <summary>
+        /// lblFeedbackTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFeedbackTitle;
+
+        /// <summary>
         /// ulFeedback control.
         /// </summary>
         /// <remarks>
